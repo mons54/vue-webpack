ce7d100f456dba51121e84619af4e7f14575fda9
Julien Simonet <j.mons54@gmail.com>
2018-04-20 17:04:18 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+import VueLivePreview from 'vue-live-preview'
+
+Vue.use(VueLivePreview, {
+  theme: 'material'
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
